303140bd9e90dbdf5f693b42a9bd1bb3509a644a
Matt Hartley <m4tthartley@gmail.com>
2025-08-22 02:22:46 +0100

    Basic decoding reg to reg mov

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@
 ##  Copyright 2025 GiantJelly. All rights reserved.
 ##
 
+set -e
+
 outdir="./build"
-mkdir -p ./build
+mkdir -p $outdir
 
 files="emu8086.c"
-flags="-I../core"
+flags="-I../core -framework CoreFoundation"
 
 clang $files $flags -o $outdir/8086 
+
+$outdir/8086 
--- emu8086.c
+++ emu8086.c
@@ -11,12 +11,97 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define CORE_IMPL
+#include <core/sys.h>
+#include <core/core.h>
+
+
+enum {
+	OP_MOV = 0b10001000,
+} opcode_t;
+
+char* regNames[] = {
+	"al",
+	"cl",
+	"dl",
+	"bl",
+	"ah",
+	"ch",
+	"dh",
+	"bh",
+
+	"ax",
+	"cx",
+	"dx",
+	"bx",
+	"sp",
+	"bp",
+	"si",
+	"di",
+};
+// char* regNamesWide[] = {
+	
+// };
+
+
+// void DecodeInstruction(uint8_t** data)
+// {
+
+// }
+
+typedef struct {
+	uint8_t* data;
+	size_t size;
+} data_t;
+
+data_t LoadFile(char* filename)
+{
+	file_t bin = sys_open(filename);
+	if (!bin) {
+		print_err("Failed to open %s \n", filename);
+	}
+
+	data_t result;
+	stat_t info = sys_stat(bin);
+	printf("file size: %lu \n\n", info.size);
+	result.data = malloc(info.size);
+	result.size = info.size;
+	sys_read(bin, 0, result.data, info.size);
+	sys_close(bin);
+
+	return result;
+}
 
 int main()
 {
-	int x = 5;
+	// data_t file = LoadFile("./data/listing_0037_single_register_mov");
+	data_t file = LoadFile("./data/listing_0038_many_register_mov");
 
-	printf("Hello World \n");
+	uint8_t* ip = file.data;
+	while (ip < file.data+file.size) {
+		if ((*ip & 0b11111100) == OP_MOV) {
+			bool d = *ip & 0b10;
+			bool w = *ip & 0b01;
+			++ip;
+
+			uint8_t mod = (*ip & 0b11000000) >> 6;
+			uint8_t reg = (*ip & 0b00111000) >> 3;
+			uint8_t rm = *ip & 0b00000111;
+			uint8_t dest = d ? reg : rm;
+			uint8_t src = d ? rm : reg;
+			++ip;
+
+			// print("mov d=%u, w=%u \n", d, w);
+			print("mov %s, %s \n", regNames[w*8 + dest], regNames[w*8 + src]);
+
+			continue;
+		}
+
+		sys_print_err("error \n");
+	}
+
 
 	return 0;
 }
